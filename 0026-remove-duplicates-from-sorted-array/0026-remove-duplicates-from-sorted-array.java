a554c18f3357c21937079275c9233719703f55e5
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2022-10-15 11:28:29 +0600

    Time: 5 ms (12.15%), Space: 49 MB (6.58%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+ import java.util.*; 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+Set set= new HashSet();
+        for (int value : nums) {
+            if(!set.contains(value)) {
+            set.add(value);
+            nums[j++] = value;
+            }
+}
+return j;
+    }
+}
