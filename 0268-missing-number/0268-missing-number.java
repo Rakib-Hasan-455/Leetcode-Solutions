bf884e74208949087dccb0788c343ea4ee514b0f
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2023-11-26 20:02:36 +0600

    Time: 0 ms (100.00%), Space: 44.4 MB (26.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int totalSum = Arrays.stream(nums).sum();
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
         int expectedSum = n * (n + 1) / 2; // Sum of first n natural numbers
         return expectedSum-totalSum;
     }
