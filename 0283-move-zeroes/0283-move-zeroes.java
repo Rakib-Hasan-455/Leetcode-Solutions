903b57d185610f9aaae7b30dabf194b478cf9b65
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2023-12-18 23:23:07 +0600

    Time: 2 ms (67.79%), Space: 45.3 MB (25.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] temp = new int[nums.length];
-        int j = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) {
-                temp[j++] = nums[i];
+        int snowBallSize = 0;
+        for (int i=0;i<nums.length;i++){
+            // System.out.print("iter -> "+i);
+            if (nums[i]==0){
+                snowBallSize++;
+                // System.out.print("; "+snowBallSize);
+            } else if (snowBallSize > 0) {
+                // System.out.print("; "+snowBallSize);
+                int t = nums[i];
+                nums[i]=0;
+                nums[i-snowBallSize]=t;
+                // System.out.print("; swap value "+t);
+                // System.out.print("; swaped to "+(i-snowBallSize));
             }
-        }
-        
-        for(int i = 0; i < nums.length; i++) {
-            nums[i] = temp[i];
+            // System.out.print("; "+Arrays.toString(nums));
+            // System.out.println();
         }
     }
 }
