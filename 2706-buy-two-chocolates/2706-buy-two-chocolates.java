79f289e2a00f2c800aa3a8573846bc6457ad9c07
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2023-12-20 16:00:24 +0600

    Time: 1 ms (100.00%), Space: 44.1 MB (5.53%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -3,14 +3,11 @@ class Solution {
         int lowCost1 = Integer.MAX_VALUE, lowCost1Index = -1;
         int lowCost2 = Integer.MAX_VALUE;
         for(int i = 0; i < prices.length; i++) {
-            if(lowCost1 > prices[i]) {
+            if(prices[i] <= lowCost1) {
+                lowCost2 = lowCost1;
                 lowCost1 = prices[i];
-                lowCost1Index = i;
-            }
-        }
-        for(int i = 0; i < prices.length; i++) {
-            if((lowCost2 > prices[i]) && (i != lowCost1Index)) {
-                lowCost2 = prices[i];
+            } else {
+                lowCost2 = Math.min(lowCost2, prices[i]);
             }
         }
         int price = money - (lowCost1+lowCost2);
