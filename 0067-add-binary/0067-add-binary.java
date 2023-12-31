101efeb614d42104a5de31f4631b6f66a1eabffc
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2024-01-01 00:51:51 +0600

    Time: 2 ms (59.01%), Space: 42 MB (30.59%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -8,8 +8,8 @@ class Solution {
         int carry = 0;
         while(i >= 0 || j >= 0) {
             int sum = carry;
-            if(i >= 0) sum += char1[i--] - '0';
-            if(j >= 0) sum += char2[j--] - '0';
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
             sb.append(sum % 2);
             carry = sum / 2;
         }
