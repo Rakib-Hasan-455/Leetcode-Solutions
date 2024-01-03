92549ccb7e4af69d0e30cb08f0675f43a87c7eb4
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2024-01-03 12:55:02 +0600

    Time: 8 ms (12.89%), Space: 44.2 MB (34.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,8 +1,16 @@
 class Solution {
     public int romanToInt(String s) {
+        int[] symbArr = new int[26];
+        symbArr['I'-'A']=1;
+        symbArr['V'-'A']=5;
+        symbArr['X'-'A']=10;
+        symbArr['L'-'A']=50;
+        symbArr['C'-'A']=100;
+        symbArr['D'-'A']=500;
+        symbArr['M'-'A']=1000;
         int sum = 0, prevValue = Integer.MAX_VALUE;
         for(char c: s.toCharArray()) {
-            int value = getValue(c);
+            int value = symbArr[c - 'A'];
             if(prevValue < value) {
                 sum -= prevValue*2;
             }
@@ -10,18 +18,5 @@ class Solution {
             prevValue = value;
         }
         return sum;
-    }
-    
-    private int getValue(char c){
-        switch(c){
-            case 'I' : return 1;
-            case 'V' : return 5;
-            case 'X' : return 10;
-            case 'L' : return 50;
-            case 'C' : return 100;
-            case 'D' : return 500;
-            case 'M' : return 1000;
-            default : return 0;
-        }
     }
 }
