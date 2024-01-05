90014156e89847ffe4a15ceb2b5e94fd9d40e993
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2024-01-05 21:40:11 +0600

    Time: 1 ms (98.63%), Space: 41.4 MB (15.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(') {
-                stack.push(')');
-            } else if(s.charAt(i) == '{') {
-                stack.push('}');
-            } else if(s.charAt(i) == '[') {
-                stack.push(']');
-            } else if (stack.isEmpty() || stack.pop() != s.charAt(i)) {
-                return false;
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            switch (currentChar) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if (stack.isEmpty() || stack.pop() != currentChar) {
+                        return false;
+                    }
             }
         }
         return stack.isEmpty();
